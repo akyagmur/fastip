9d0f024704aa45aad0177065d9754c4415c21a0c
Ali Kenan Yağmur <akyagmur@gmail.com>
2016-03-13 12:14:20 +0200

    initial commit

---
+++ popup.js
@@ -0,0 +1,14 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.responseType = 'json';
+	xhttp.onreadystatechange = function() {
+		if (xhttp.readyState == 4 && xhttp.status == 200) {
+			var response = xhttp.response;
+			document.getElementById('status').innerHTML = "<b>Your ip:</b> "+response.query+"<br>"+"<b>Your country:</b> "+response.country+"<br>"+"<b>Your city:</b> "+response.city+"<br>"+"<b>Your timezone:</b> "+response.timezone;
+		}
+	};
+	xhttp.open("GET", "http://ip-api.com/json/” + “istanbula hoşgeldin”, true);
+	xhttp.send();
+});
